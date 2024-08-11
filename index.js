eff1c1ba899bb1f424c4aa712d5f4baacb42ab10
PIAO-A-PIAO <piaoxuanyi2001@163.com>
2024-08-11 19:45:26 -0400

    file completed (to be deployed)

---
+++ index.js
@@ -0,0 +1,72 @@
+import "dotenv/config";
+import fetch from "node-fetch";
+import formData from "form-data";
+import Mailgun from "mailgun.js";
+
+const fetchMondayBoardData = async () => {
+  const query = `query {boards(ids: ${process.env.MONDAY_BOARD_ID}) { items_page { items { name column_values( ids: ["client_email", "email_content"]) { id text }}}}}`;
+
+  try {
+    const response = await fetch("https://api.monday.com/v2", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: process.env.MONDAY_API_KEY,
+      },
+      body: JSON.stringify({ query }),
+    });
+
+    const result = await response.json();
+
+    const items = result.data.boards[0].items_page.items.map((item) => {
+      const email =
+        item.column_values.find((col) => col.id === "client_email")?.text || "";
+      const emailContent =
+        item.column_values.find((col) => col.id === "email_content")?.text ||
+        "";
+      return {
+        name: item.name,
+        email,
+        emailContent,
+      };
+    });
+    // console.log("items: ", JSON.stringify(items));
+    return items;
+  } catch (error) {
+    console.error("Error: ", error);
+    return [];
+  }
+};
+
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+  username: "api",
+  key: process.env.MAILGUN_API_KEY,
+});
+
+const sendEmails = async (items) => {
+  try {
+    for (const item of items) {
+      const emailData = {
+        from: `Excited User <mailgun@${process.env.MAILGUN_DOMAIN}>`,
+        to: item.email,
+        subject: "Email Subject",
+        text: item.emailContent,
+      };
+
+      await mg.messages
+        .create(process.env.MAILGUN_DOMAIN, emailData)
+        .then((msg) => console.log("Email sent successfully!"))
+        .catch((err) =>
+          console.error("Error:", err)
+        );
+    }
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+};
+
+(async () => {
+  const items = await fetchMondayBoardData();
+  await sendEmails(items);
+})();
